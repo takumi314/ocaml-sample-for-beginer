19ebff04fb0318e018e9c4f168fe6a6c55adf72d
takumi314 <isospin_symmetry@me.com>
2017-12-18 02:25:29 +0900

    fix indents

--- fold_right.ml
+++ fold_right.ml
@@ -7,7 +7,9 @@ let rec fold_right f lst init = match lst with
 
 (* 目的 : first と rest_result を加える *)
 (* add_int : int -> int -> int  *)
-(* let rec add_int first rest_result = first + rest_result *)
+(*
+  let rec add_int first rest_result = first + rest_result
+*)
 
 (* 目的 : 受け取ったリスト lst の各要素の和を求める *)
 (* sum : int list -> int *)
@@ -16,15 +18,19 @@ let rec sum lst =
   in fold_right add_int lst 0
 
 (* テスト *)
-(* let test0 = sum [] = 0
-let test1 = sum [2] = 2
-let test2 = sum [1; 3] = 4
-let test3 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55 *)
+(*
+  let test0 = sum [] = 0
+  let test1 = sum [2] = 2
+  let test2 = sum [1; 3] = 4
+  let test3 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55
+*)
 
 
 (* 目的 : firstは無視して rest_result に1を加える *)
 (* add_one : int -> int -> int *)
-(* let add_one first rest_result = 1 + rest_result *)
+(*
+  let add_one first rest_result = 1 + rest_result
+*)
 
 (* 目的 : 受け取ったリスト lst の長さを求める *)
 (* length : 'a list -> int *)
@@ -43,12 +49,16 @@ let test10 = length ["a"; "b"; "c"; "d"] = 4
 
 (* 目的 : first を リスト lst_result の先頭に加える *)
 (* cons : 'a -> 'a lsit -> 'a list *)
-(* let rec cons first lst_result = first :: lst_result *)
+(*
+  let rec cons first lst_result = first :: lst_result
+*)
 
 (* テスト *)
-(* let test11 = cons 1 [] = [1]
-let test12 = cons 1 [2] = [1; 2]
-let test13 = cons 1 [2; 3] = [1; 2; 3] *)
+(*
+  let test11 = cons 1 [] = [1]
+  let test12 = cons 1 [2] = [1; 2]
+  let test13 = cons 1 [2; 3] = [1; 2; 3]
+*)
 
 (* 目的 : lst1 と lst2 を受け取って, それらを結合したリストを返す *)
 (* append : 'a list -> 'a list -> 'a list *)
